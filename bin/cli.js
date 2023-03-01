e93c0a081390608ad6363aea5e4df122f339f00e
xing-6464 <xing_6464@foxmail.com>
2023-03-02 06:02:44 +0800

    使用commander处理help选项

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
-if (process.argv[2] === '--help') {
-  console.info(process.argv[2])
-}
+const { program } = require('commander')
+
+program.option('-f --framwork <framwork>', '设置框架')
+program.parse(process.argv)
