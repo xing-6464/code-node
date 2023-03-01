b664e65c6b53340528dceb47e197f05e3a105b38
xing-6464 <xing_6464@foxmail.com>
2023-03-02 06:22:51 +0800

    逻辑代码模块化拆分

--- bin/cli.js
+++ bin/cli.js
@@ -2,15 +2,12 @@
 
 const { program } = require('commander')
 
-program.option('-f --framwork <framwork>', '设置框架')
-program
-  .command('create <project> [other...]')
-  .alias('crt')
-  .description('创建项目')
-  .action((project, args) => {
-    // 命令行执行代码
-    console.info(project)
-    console.info(args)
-  })
+const myhelp = require('../lib/core/help')
+const mycommander = require('../lib/core/mycommander')
+
+// help option
+myhelp(program)
+// 命令
+mycommander(program)
 
 program.parse(process.argv)
