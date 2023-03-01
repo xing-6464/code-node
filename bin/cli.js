f60805a4d7f2f05253e0ce0cec1db4e0faa57418
xing-6464 <xing_6464@foxmail.com>
2023-03-02 06:13:05 +0800

    处理自定义指令选线

--- bin/cli.js
+++ bin/cli.js
@@ -3,4 +3,14 @@
 const { program } = require('commander')
 
 program.option('-f --framwork <framwork>', '设置框架')
+program
+  .command('create <project> [other...]')
+  .alias('crt')
+  .description('创建项目')
+  .action((project, args) => {
+    // 命令行执行代码
+    console.info(project)
+    console.info(args)
+  })
+
 program.parse(process.argv)
