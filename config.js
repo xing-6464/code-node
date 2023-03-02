eb785237e547aa17b56a66b73e465bc9c973d37a
xing-6464 <xing_6464@foxmail.com>
2023-03-02 19:58:16 +0800

    下载远程仓库模板代码

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  // 可选择的框架
   framwork: ['express', 'koa', 'egg'],
+  // 框架对应的下载地址
+  foramworkUrl: {
+    express: 'git@gitee.com:xing_6464/express-template.git',
+    koa: 'git@gitee.com:xing_6464/koa-template.git',
+    egg: 'git@gitee.com:xing_6464/egg-template.git',
+  },
 }
